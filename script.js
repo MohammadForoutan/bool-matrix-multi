50bff1b0023c499f5ff772c97e084c715153a46e
Mohammad <mmdfrn81@gmail.com>
2022-04-30 19:41:37 +0430

    title

--- script.js
+++ script.js
@@ -21,10 +21,8 @@ function calcMatrix() {
 		.split("\n")
 		.map((t) => t.trim().split(" "));
 	// set columns and row
-	Aj = A.length;
-	Bj = B.length;
 	Ai = A[0].length;
-	Bi = B[0].length;
+	Aj = A.length;
 	// solve multiply
 	multiply(Aj, Ai, A, Bj, Bi, B);
 
@@ -34,15 +32,15 @@ function calcMatrix() {
 	DrawResult();
 }
 
-function multiply(Aj, Ai, A, Bj, Bi, B) {
+function multiply(Aj, Ai, A, B) {
 	// temp variables
 	let x, i, j;
 
 	res = new Array(Aj);
-	for (i = 0; i < Aj; i++) res[i] = new Array(Bi);
+	for (i = 0; i < Aj; i++) res[i] = new Array(Aj);
 
 	for (i = 0; i < Aj; i++) {
-		for (j = 0; j < Bi; j++) {
+		for (j = 0; j < Ai; j++) {
 			res[i][j] = 0;
 			for (x = 0; x < Ai; x++) {
 				let val = A[i][x] * B[x][j];
@@ -56,8 +54,8 @@ function multiply(Aj, Ai, A, Bj, Bi, B) {
 }
 
 function DrawResult() {
-	const con = document.querySelector(".container")
-    con.style.opacity = '1'
+	const con = document.querySelector(".container");
+	con.style.opacity = "1";
 	const ans = document.querySelector(".ans");
 	ans.innerHTML = "";
 	// Draw matrix on screen
@@ -68,7 +66,9 @@ function DrawResult() {
 			let tmp;
 			if (res[i][j] > 1) {
 				tmp = 1;
-			} else { tmp = res[i][j]}
+			} else {
+				tmp = res[i][j];
+			}
 			ans.innerHTML += tmp + " ";
 		}
 		ans.innerHTML += "<br>";
