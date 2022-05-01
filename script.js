e69aa13310541090c647877bfaebe40271937e68
Mohammad <mmdfrn81@gmail.com>
2022-05-01 12:37:18 +0430

    v1

--- script.js
+++ script.js
@@ -2,51 +2,26 @@
 const inputA = document.getElementById("input-a");
 const inputB = document.getElementById("input-b");
 const btn = document.getElementById("btn");
+const con = document.querySelector(".container");
 btn.addEventListener("click", calcMatrix);
 
-let A, B, res;
-
+let A, B, res, isValidate;
 let Aj, // Matrix A columns
 	Ai, // Matrix A rows
 	Bj, // Matrix B columns
 	Bi; // Matrix B rows
-function calcMatrix() {
-	// get matrix(text) and convert them to array
-	A = inputA.value
-		.trim()
-		.split("\n")
-		.map((t) => t.trim().split(" "));
-	B = inputB.value
-		.trim()
-		.split("\n")
-		.map((t) => t.trim().split(" "));
-	// set columns and row
-	Ai = A[0].length;
-	Aj = A.length;
-	// solve multiply
-	multiply(Aj, Ai, A, Bj, Bi, B);
 
-	// draw it
-	drawA();
-	drawB();
-	DrawResult();
-}
+function multiply(Aj, Ai, A, Bj, Bi, B) {
+	res = new Array(Ai);
+	for (let i = 0; i < Ai; i++) res[i] = new Array(Bj);
 
-function multiply(Aj, Ai, A, B) {
-	// temp variables
-	let x, i, j;
+	console.log({ res, Aj, Ai, A, Bj, Bi, B});
 
-	res = new Array(Aj);
-	for (i = 0; i < Aj; i++) res[i] = new Array(Aj);
-
-	for (i = 0; i < Aj; i++) {
-		for (j = 0; j < Ai; j++) {
+	for (i = 0; i < Ai; i++) {
+		for (j = 0; j < Bj; j++) {
 			res[i][j] = 0;
-			for (x = 0; x < Ai; x++) {
-				let val = A[i][x] * B[x][j];
-				if (val > 1) {
-					val = 1;
-				}
+			for (x = 0; x < Aj; x++) {
+				val = A[i][x] * B[x][j];
 				res[i][j] += val;
 			}
 		}
@@ -54,15 +29,13 @@ function multiply(Aj, Ai, A, B) {
 }
 
 function DrawResult() {
-	const con = document.querySelector(".container");
 	con.style.opacity = "1";
 	const ans = document.querySelector(".ans");
 	ans.innerHTML = "";
 	// Draw matrix on screen
 
-	console.log({ res });
-	for (i = 0; i < Aj; i++) {
-		for (j = 0; j < Bi; j++) {
+	for (i = 0; i < Ai; i++) {
+		for (j = 0; j < Bj; j++) {
 			let tmp;
 			if (res[i][j] > 1) {
 				tmp = 1;
@@ -79,8 +52,8 @@ function drawA() {
 	const matrixA = document.querySelector(".m-a");
 	matrixA.innerHTML = "";
 	// Draw matrix on screen
-	for (i = 0; i < Aj; i++) {
-		for (j = 0; j < Ai; j++) {
+	for (i = 0; i < Ai; i++) {
+		for (j = 0; j < Aj; j++) {
 			matrixA.innerHTML += A[i][j] + " ";
 		}
 		matrixA.innerHTML += "<br>";
@@ -92,10 +65,46 @@ function drawB() {
 	matrixB.innerHTML = "";
 
 	// Draw matrix on screen
-	for (i = 0; i < Bj; i++) {
-		for (j = 0; j < Bi; j++) {
+	for (i = 0; i < Bi; i++) {
+		for (j = 0; j < Bj; j++) {
 			matrixB.innerHTML += B[i][j] + " ";
 		}
 		matrixB.innerHTML += "<br>";
 	}
+}
+
+function calcMatrix() {
+	con.style.opacity = "0";
+	// get matrix(text) and convert them to array
+	A = inputA.value
+		.trim()
+		.split("\n")
+		.map((t) => t.trim().split(" "));
+	B = inputB.value
+		.trim()
+		.split("\n")
+		.map((t) => t.trim().split(" "));
+	console.log({ A, B });
+	// set columns and row
+	Aj = A[0].length;
+	Ai = A.length;
+	Bj = B[0].length;
+	Bi = B.length;
+	// validate matrix
+	if(Aj === Bi) {
+		isValidate = true;
+	} else {
+		isValidate = false;
+	}
+	console.log({ isValidate });
+
+	// solve multiply
+	multiply(Aj, Ai, A, Bj, Bi, B);
+
+	// draw it
+	drawA();
+	drawB();
+	setTimeout(() => {
+		DrawResult();
+	}, 300);
 }
